228d0be41fa2da5e29bfeae0065fd165f1d36bc2
ponypharmacist <evilglen@gmail.com>
2017-11-08 23:49:43 +0300

    Поиск, рендер
    Добавлен поиск по узлам, рендер их описаний по клику до первых детей.

--- app/js/app.js
+++ app/js/app.js
@@ -2,44 +2,74 @@ var treeHtml = '';
 
 function renderTree() {
   abz.forEach(function(megauzel, i, abz) {
-    treeHtml += '<li><a class="uzel-link" href="#">' + megauzel.name + '</a>'
-    treeHtml += megauzel.children && renderChildren(megauzel);
-    treeHtml += '</li>'
+    treeHtml += '<li><span>' + (i + 1) + '.</span>'
+    treeHtml += '<a onclick="renderNode(' + i + ');">' + megauzel.name + '</a>'
+    treeHtml += megauzel.children ? renderChildren(megauzel, i) : '</li>';
   });
 
   $('#tree-root').html(treeHtml);
 };
 
-function renderChildren(uzel) {
+function renderChildren(uzel, parentIndex) {
   let thisUzelChildren = uzel.children;
   let thisUzelId = uzel.id;
-  let childrenHtml = '&nbsp;<a class="expand-link" href="#" rel="' + thisUzelId + '">[+]</a>';
+  let childrenHtml = '&nbsp;<a class="expand-link" rel="' + thisUzelId + '">[+]</a></li>';
   childrenHtml += '<ul id="childrenof-' + thisUzelId + '">'
-  thisUzelChildren.forEach(function(child, i, thisUzelChildren) {
-    childrenHtml += '<li><a href="#">' + child.name + '</a></li>'
+  thisUzelChildren.forEach(function(child, k, thisUzelChildren) {
+    childrenHtml += '<li><a onclick="renderNode(' + parentIndex + ',' + k + ');">' + child.name + '</a></li>'
   });
   childrenHtml += '</ul>'
   return childrenHtml;
 };
 
-function renderNode(descriptionText) {
-  // Обновить хлебокрошки
-  // Обновить описание
-  $('#nodeName').html(name);
-  $('#nodeConstructionNumber').html(constructorNumber);
-  $('#nodeCatalogNumber').html(catalogNumber);
-  $('#nodeDescription').html(description);
+function renderNode(i, k) {
+  let target = '';
+  if (i >= 0) {
+    console.log('First condition');
+    target = abz[i];
+  } else if (i >= 0 && k >= 0) {
+    target = abz[i].children[k];
+  } else {};
+  $('#nodeName').html(target.name);
+  $('#nodeConstructionNumber').html(target.constructorNumber ? target.constructorNumber : '');
+  $('#nodeCatalogNumber').html(target.catalogNumber ? target.catalogNumber : '');
+  $('#nodeDescription').html(target.description ? target.description : '');
 };
 
 $(document).ready(function(){
   // Рендерим список узлов
   renderTree();
 
-  $('.expand-link').click(function(){
+  $('.expand-link').on('click', function(){
     $('#childrenof-' + $(this).attr('rel')).toggle();
     $(this).toggleClass('expanded');
     if ($(this).hasClass('expanded')) {
       $(this).html('[-]');
     } else { $(this).html('[+]'); };
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+      $('#tree-root').removeClass('being-filtered');
+      $('#tree-root li').css('display', '');
+      $('.filter-hint').css('display', 'none');
+    } else {};
+  });
+
+  $('#filter-input').on('keyup', function(){
+    $('#tree-root').addClass('being-filtered');
+    $('.filter-hint').css('display', 'inline-block');
+    let a, i;
+    let filter = $(this).val().toUpperCase();
+    let li = $('#tree-root li');
+
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
   });
 });
--- app/js/data.js
+++ app/js/data.js
@@ -20,31 +20,31 @@ var abz = [
         name: 'Затвор',
         description: '(описание детали)',
         constructorNumber: '04АБЗ-001.03.00.000СБ',
-        catalogNumber: '4.1.3.'
+        catalogNumber: '4.1.3'
       },
       { id: 'id0102',
         name: 'Затвор',
         description: '(описание детали)',
         constructorNumber: '04АБЗ-001.04.00.000СБ',
-        catalogNumber: '4.1.4.'
+        catalogNumber: '4.1.4'
       },
       { id: 'id0103',
         name: 'Кронштейн',
         description: '(описание детали)',
         constructorNumber: '04АБЗ-001.06.00.000СБ',
-        catalogNumber: '4.1.6.'
+        catalogNumber: '4.1.6'
       },
       { id: 'id0104',
         name: 'Кронштейн',
         description: '(описание детали)',
         constructorNumber: '04АБЗ-001.07.00.000СБ',
-        catalogNumber: '4.1.7.'
+        catalogNumber: '4.1.7'
       },
       { id: 'id0105',
         name: 'Подшипник',
         description: '(описание детали)',
         constructorNumber: '',
-        catalogNumber: '4.1.49.'
+        catalogNumber: '4.1.49'
       },
       { id: 'id0106',
         name: 'Пневмоцилиндр',
@@ -63,7 +63,65 @@ var abz = [
     name: 'Бункер негабарита',
     description: '(описание детали)',
     children: ''
+  },
+  { id: 'id04',
+    name: 'Мешалка',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id05',
+    name: 'Редуктор',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id06',
+    name: 'Весы битумные',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id07',
+    name: 'Весы пыли',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id08',
+    name: 'Весы минерала',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id09',
+    name: 'Весы ВИАТОПа',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id10',
+    name: 'Бункер минерала',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id11',
+    name: 'Бункер пыли',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id12',
+    name: 'Короб грохота',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id13',
+    name: 'Вибратор',
+    description: '(описание детали)',
+    children: ''
+  },
+  { id: 'id14',
+    name: 'Устройство распределительное',
+    description: '(описание детали)',
+    children: ''
   }
 ] // end Megauzels array
   // Получать родителя для узлов автоматически, цепочку родителей, хранить в строковом параметре и выдавать в хлебокрошки целиком? Возможно
-  // Выбор узла из маркера на чертеже, как открыть целевой элемент дерева снаружи, без кликов по дереву?
+  // === Выбор узла из маркера на чертеже, ===
+  //     как открыть целевой элемент дерева снаружи, без кликов по дереву?
+
+  // Подсветка открытого узла
